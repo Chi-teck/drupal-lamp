6e0558a7f35ea242fa084ccf767b3e58e054ac84
Chi-teck <Chi-teck@users.noreply.github.com>
2019-10-05 09:23:22 +0500

    Add context providers to dumper

--- dumper.php
+++ dumper.php
@@ -2,7 +2,11 @@
 
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 use Symfony\Component\VarDumper\Dumper\ServerDumper;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\RequestContextProvider;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
 use Symfony\Component\VarDumper\VarDumper;
+
 
 if (!class_exists('Symfony\Component\VarDumper\Dumper\ServerDumper')) {
   require_once '/opt/var-dumper/vendor/autoload.php';
@@ -11,6 +15,13 @@ if (!class_exists('Symfony\Component\VarDumper\Dumper\ServerDumper')) {
 if (!function_exists('dumps')) {
   function dumps($var, ...$moreVars) {
     $cloner = new VarCloner();
+    $contextProviders = [
+      'cli' => new CliContextProvider(),
+      'source' => new SourceContextProvider(),
+    ];
+    if (PHP != 'cli' && class_exists('\Drupal')) {
+      $contextProviders['request'] = new RequestContextProvider(\Drupal::requestStack());
+    }
     $dumper = new ServerDumper($_SERVER['VAR_DUMPER_SERVER'] ?? '127.0.0.1:9912');
     $handler = function ($var) use ($cloner, $dumper) {
       $dumper->dump($cloner->cloneVar($var));
